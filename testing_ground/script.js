8f210b4fd9e0a9ee006172b7a10e8ed01672c456
antony-lucide <113905621+antony-lucide@users.noreply.github.com>
2023-09-24 00:20:30 +0200

    Update script.js
    ajout d'une liste de tâche pour ajouter

--- testing_ground/script.js
+++ testing_ground/script.js
@@ -1,30 +1,42 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     let todolist = document.querySelectorAll("p")
     let btn = document.querySelector("#bouton")
+
+    datalist = []
     btn.addEventListener("click", (event) => {
-       event.preventDefault()
-       let todoHTML = "";
+        event.preventDefault()
+        let todoHTML = ""
 
-       todolist.forEach(todolist => {
-        todoHTML += todolist.innerHTML;
-    });
+        todolist.forEach(paragraphe => {
+            todoHTML += paragraphe.innerHTML
+        })
 
-       fetch(`traitement.php?param1=${encodeURIComponent(todoHTML)}`, {
-           method: 'GET',
-        //    headers: {
-        //        'Content-Type': 'application/x-www-form-urlencoded',
-           },
-        //    body: 'todoHTML=' + encodeURIComponent(todolist.innerHTML))
-       )
+        fetch(`traitement.php?param1=${encodeURIComponent(todoHTML)}`, {
+            method: 'GET'
+        })
         .then(response => {
-        return response.text();
+            return response.text();
         })
         .then(data => {
             console.log('Data received:', data.slice(3));
         })
         .catch(error => {
             console.error('Error:', error);
-        });
-})
+        })
+    })
 
+    let add = document.querySelector("#add");
+    let replacement = document.querySelector("#todoAdd");
+
+    add.addEventListener("click", () => {
+        let replacementText = replacement.value
+
+        todolist.forEach((paragraphe, i) => {
+            let result = paragraphe.innerHTML.replace(/{{placeholder}}/g, replacementText)
+            paragraphe.innerHTML = result
+
+            datalist[i] = result
+            console.log(datalist[i])
+        })
+    })
 })
