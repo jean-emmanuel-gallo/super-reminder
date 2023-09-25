d755f465bd0caa14be70072b451da14dbb0fb9ea
antony-lucide <113905621+antony-lucide@users.noreply.github.com>
2023-09-25 11:46:35 +0200

    Update login.php
    mise à jour connexion base de données

--- testing_ground/login.php
+++ testing_ground/login.php
@@ -46,3 +46,44 @@
     </footer>
 </body>
 </html>
+
+
+<?php 
+    class Database {
+        public string $userdb = "root";
+        public string $password = ""; 
+
+        //Connection à la database
+        public function register($login,$password){
+            $pdo = new PDO('mysql:host=localhost;dbname=moduleconnexionb2;charset=utf8','root','');
+
+            $stm = $pdo->prepare("INSERT INTO user (login,password) VALUES (:login,:password)");
+            $stm->bindValue(':login', $login, PDO::PARAM_STR);
+            $stm->bindValue(':password', $password, PDO::PARAM_STR);
+            $stm->execute();
+        }
+        // hasher le mdp 
+        // mettre les classes et le html dans des fichiers différent 
+        
+            // private function passwordverify($login,$password){
+            //     if(strlen($email) > 8){
+            //         return true;
+            //     }
+            //     elseif(strlen($password) > 8){
+            //         return true;
+            //     }
+            //     else{
+            //         return false;
+            //     }
+            // }
+    }
+//envoie du formulaire
+    if(isset($_POST["submit"])){
+        $login = $_POST["login"];
+        $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+        echo "Succès";
+        $databaseInstances = new Database();
+        $databaseInstances->register($login,$password);
+    }
+
+?>
