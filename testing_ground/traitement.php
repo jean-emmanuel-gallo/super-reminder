c678bad5aafb495c8cc6d2945630947d591e5032
antony-lucide <113905621+antony-lucide@users.noreply.github.com>
2023-10-07 19:08:02 +0200

    Update traitement.php
    traitement de tache

--- testing_ground/traitement.php
+++ testing_ground/traitement.php
@@ -1,3 +1,20 @@
 <?php
-var_dump($_GET["param1"]); 
+require("Database.php")
+class traitement extends Database{
+    session_start();
+    public $task;
+    
+    $task = $_GET["param1"];
+
+    public function __construct() {
+        parent::__construct();
+        $this->task = $task;
+    }
+
+    public function task(){
+        $require = $this->$db->prepare("INSERT INTO $_SESSION["id"] (`task`) VALUES ($task)");
+        $require->bindParam(':task', $this->task, PDO::PARAM_STR);
+        $require->execute();
+    }
+}
 ?>
